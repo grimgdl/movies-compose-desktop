5f3ef9e1dd90cdabe93c4b458f2cd6e55c666d32
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2024-06-25 14:00:11 -0600

    added a properties file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,10 +8,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.*
 import ui.pages.LoginScreen
 import ui.pages.MoviesScreen
 import ui.pages.NothingScreen
@@ -86,9 +87,13 @@ enum class Screens {
 
 
 fun main() = application {
+
+
     Window(
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
+        state = WindowState(position = WindowPosition(alignment = Alignment.Center))
     ) {
+
 
         App()
     }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,6 +1,56 @@
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.*
+
 object Utils {
 
     const val KEY = "122fb00f"
 
     const val URL = "http://www.omdbapi.com/?apikey=[yourkey]&"
+
+
+
+    fun createDefaultProperties(filePath: String) {
+        val file = File(filePath)
+
+        if (!file.exists()) {
+            val properties = Properties().apply {
+                setProperty("window.position.x", "100")
+                setProperty("window.position.y", "100")
+            }
+
+            saveProperties(filePath, properties)
+        }
+
+
+    }
+
+
+    fun loadProperties(filePath: String): Properties {
+        val properties = Properties()
+        FileInputStream(filePath).use { inputStream ->
+
+            properties.load(inputStream)
+
+        }
+
+        return properties
+    }
+
+
+    private fun saveProperties(filePath: String, properties: Properties) {
+        FileOutputStream(filePath).use { outputStream ->
+            properties.store(outputStream, null)
+
+        }
+    }
+
+    fun updateProperty(filePath: String, key: String, value: String) {
+        val properties = loadProperties(filePath)
+        properties.setProperty(key, value)
+        saveProperties(filePath, properties)
+    }
+
+
 }
